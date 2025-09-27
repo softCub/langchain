6587c0e677cd1abcf4d1d611f6b402955bbdf17e
softCub <arjunsivag1@gmail.com>
2025-09-27 13:23:17 +0530

    parallel lets gooo

--- parallel.py
+++ parallel.py
@@ -1,5 +1,6 @@
 from langchain_google_genai import ChatGoogleGenerativeAI as ai
-from langchain.prompt import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnableParallel, RunnableLambda
 from dotenv import load_dotenv
 
@@ -19,6 +20,8 @@ def analyze_plot(plot):
             ("human","Analise the plot: {plot}. what are its strength and weaknesses"),
         ]
     )
+    return plot_temp.format_prompt(plot=plot)
+    
 def analyze_character(character):
     character_temp = ChatPromptTemplate.from_messages(
         [
@@ -26,9 +29,20 @@ def analyze_character(character):
             ("human","Analise the characters {character}.what are their strengths and weaknesses")
         ]
     )
+    return character_temp.format_prompt(character=character)
 
-
+def combine(plot_anal,character_anal):
+    return f"Plot Analysis:\n{plot_anal}\n------------------------------------------\nCharacter Analysis:\n{character_anal}"
+plot_branch =(
+    RunnableLambda(lambda x:analyze_plot(x))|model|StrOutputParser()
+)
+character_branch=(
+    RunnableLambda(lambda x:analyze_character(x))|model|StrOutputParser()
+)
 chain = (
     summary|model|StrOutputParser()|
-    RunnableParallel()
+    RunnableParallel(branches={"plot":plot_branch,"character":character_branch})|
+    RunnableLambda(lambda x:combine(x['branches']['plot'],x['branches']['character']))
 )
+result =chain.invoke({"movid_name":"pasific rim"})
+print(result)
